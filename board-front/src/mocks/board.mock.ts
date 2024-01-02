9105ac5537f95371a0f2db171d7f11d6d4bea991
Sooin <sooin0458@gmail.com>
2024-01-02 15:08:38 +0900

    게시물 상세 페이지 목데이터 작성 및 이벤터 처리

---
+++ board-front/src/mocks/board.mock.ts
@@ -0,0 +1,14 @@
+import { Board } from 'types/interface';
+
+const boardMock: Board = {
+  boardNumber: 1,
+  title: '제목입니다.제목입니다.제목입니다.제목입니다.제목입니다.제목입니다.제목입니다.',
+  content: '내용입니다.내용입니다.내용입니다.내용입니다.내용입니다.내용입니다.내용입니다.내용입니다.내용입니다.내용입니다.',
+  boardImageList: ['https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202306/25/488f9638-800c-4bac-ad65-82877fbff79b.jpg', 'https://cdn.ccdailynews.com/news/photo/202104/2050720_530952_025.png'],
+  writeDatetime: '2024. 01. 02',
+  writerEmail: 'email@email.com',
+  writerNickname: '안녕하세요나는조코야키',
+  writerProfileImage: 'https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg',
+}
+
+export default boardMock;
