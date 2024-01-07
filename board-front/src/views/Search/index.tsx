b9541b930a7a2d94077cdda777d68db5cffa4ed4
Sooin <sooin0458@gmail.com>
2024-01-07 17:26:19 +0900

    검색 페이지 API 연동

--- board-front/src/views/Search/index.tsx
+++ board-front/src/views/Search/index.tsx
@@ -6,31 +6,65 @@ import { latestBoardListMock } from 'mocks';
 import BoardItem from 'components/BoardItem';
 import { SEARCH_PATH } from 'constant';
 import Pagination from 'components/Pagination';
+import { getRelationListRequest, getSearchBoarListRequest } from 'apis';
+import { GetSearchBoardListResponseDto } from 'apis/response/board';
+import { ResponseDto } from 'apis/response';
+import { usePagination } from 'hooks';
+import { GetRelationListResponseDto } from 'apis/response/search';
 
 //          component: 검색 화면 컴포넌트          //
 export default function Search() {
 
   //          state: searchWord path variable 상태          //
   const { searchWord } = useParams();
+  //          state: 페이지네이션 관련 상태          //
+  const {
+    currentPage, currentSection, viewList, viewPageList, totalSection,
+    setCurrentPage, setCurrentSection, setTotalList
+  } = usePagination<BoardListItem>(5);
   //          state: 검색 게시물 개수 상태          //
   const [count, setCount] = useState<number>(0);
-  //          state: 검색 게시물 리스트 상태(임시)          //
-  const [searchBoardList, setSearchBoardList] = useState<BoardListItem[]>([]);
+  //          state: 이전 검색어 상태          //
+  const [preSearchWord, setPreSearchWord] = useState<string | null>(null);
   //          state: 관련 검색어 리스트 상태          //
-  const [relationList, setRelationList] = useState<string[]>([]);
+  const [relationWordList, setRelationWordList] = useState<string[]>([]);
 
   //          function: navigate 함수         //
   const navigate = useNavigate();
+  //          function: get search board list response 처리 함수          //
+  const getSearchBoarListResponse = (responseBody: GetSearchBoardListResponseDto | ResponseDto | null) => {
+    if (!responseBody) return;
+    const { code } = responseBody;
+    if (code === 'DBE') alert('데이터베이스 오류입니다.');
+    if (code !== 'SU') return;
+
+    if (!searchWord) return;
+    const { searchList } = responseBody as GetSearchBoardListResponseDto;
+    setTotalList(searchList);
+    setCount(searchList.length);
+    setPreSearchWord(searchWord);
+  }
+  //          function: get relation list response 처리 함수          //
+  const getRelationListResponse = (responseBody: GetRelationListResponseDto | ResponseDto | null) => {
+    if (!responseBody) return;
+    const { code } = responseBody;
+    if (code === 'DBE') alert('데이터베이스 오류입니다.');
+    if (code !== 'SU') return;
+
+    const { relativeWordList } = responseBody as GetRelationListResponseDto;
+    setRelationWordList(relativeWordList);
+  }
 
   //          event handler: 연관 검색어 클릭 이벤트 처리          //
   const onRelationWordClickHandler = (word: string) => {
     navigate(SEARCH_PATH(word));
   }
 
-  //          effect: 첫 마운트 시 실행될 함수          //
+  //          effect: search word 상태 변경 시 실행될 함수          //
   useEffect(() => {
-    setSearchBoardList(latestBoardListMock);
-    setRelationList([])
+    if (!searchWord) return;
+    getSearchBoarListRequest(searchWord, preSearchWord).then(getSearchBoarListResponse);
+    getRelationListRequest(searchWord).then(getRelationListResponse);
   }, [searchWord]);
 
   //          render: 검색 화면 컴포넌트 랜더링          //
@@ -45,16 +79,16 @@ export default function Search() {
         <div className='search-contents-box'>
           {count === 0 ? 
           <div className='search-contents-nothing'>{'검색 결과가 없습니다.'}</div> :
-          <div className='search-contents'>{searchBoardList.map(boardListItem => <BoardItem boardListItem={boardListItem} />)}</div>
+          <div className='search-contents'>{viewList.map(boardListItem => <BoardItem boardListItem={boardListItem} />)}</div>
           }
           <div className='search-relation-box'>
             <div className='search-relation-card'>
               <div className='search-relation-card-container'>
                 <div className='search-relation-card-title'>{'관련 검색어'}</div>
-                {relationList.length === 0 ? 
+                {relationWordList.length === 0 ? 
                   <div className='search-relation-card-contents-nothing'>{'관련 검색어가 없습니다.'}</div> :
                   <div className='search-relation-card-contents'>
-                    {relationList.map(word => <div className='word-badge' onClick={() => onRelationWordClickHandler(word)}>{word}</div>)}
+                    {relationWordList.map(word => <div className='word-badge' onClick={() => onRelationWordClickHandler(word)}>{word}</div>)}
                   </div>
                 }
               </div>
@@ -62,7 +96,15 @@ export default function Search() {
           </div>
         </div>
         <div className='search-pagination-box'>
-          {/* {count !== 0 &&b<Pagination /> */}
+          {count !== 0 && 
+          <Pagination 
+            currentPage={currentPage}
+            currentSection={currentSection}
+            setCurrentPage={setCurrentPage}
+            setCurrentSection={setCurrentSection}
+            viewPageList={viewPageList}
+            totalSection={totalSection}
+          />}
         </div>
       </div>
     </div>
