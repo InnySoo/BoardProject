1df80eeaa67b987c431f53965ac99234ee1311e9
Sooin <sooin0458@gmail.com>
2024-01-08 16:18:06 +0900

    유저 페이지 API 연동

---
+++ board-front/src/apis/request/user/index.ts
@@ -0,0 +1,7 @@
+import PatchNicknameRequestDto from './patch-nickname.request.dto';
+import PatchProfileImageRequestDto from './patch-profile-image.request.do';
+
+export type {
+  PatchNicknameRequestDto,
+  PatchProfileImageRequestDto
+}
